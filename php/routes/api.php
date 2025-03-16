c802e3e50530d813df6e8c7b12a5295dd397349d
TwoSug4r <nairoz0@mail.ru>
2025-03-16 03:48:12 +0300

    упростил вид api методов

--- php/routes/api.php
+++ php/routes/api.php
@@ -4,8 +4,5 @@ use App\Http\Controllers\Api\BookController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
 
-Route::get('/books', [BookController::class, 'index']);        // GET /api/books
-Route::post('/books', [BookController::class, 'store']);       // POST /api/books
-Route::get('/books/{id}', [BookController::class, 'show']);    // GET /api/books/{id}
-Route::put('/books/{id}', [BookController::class, 'update']);  // PUT /api/books/{id}
-Route::delete('/books/{id}', [BookController::class, 'destroy']); // DELETE /api/books/{id}
+Route::apiResource('books', BookController::class); //make 'index', 'show', 'store', 'update', 'destroy' methods.
+//Route::apiResource('users', UserController::class); //надо добавить контроллер под API,json.
