82aa05a257ca5a8464368007b0e3e2ca6ae2c2e2
TwoSug4r <nairoz0@mail.ru>
2025-03-17 00:23:12 +0300

    add UserController and add in this controller RESTful API methods, add Route in api.php

---
+++ php/app/Http/Controllers/Api/UserController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+    public function index(): JsonResponse
+    {
+        $users = User::all();
+        return response()->json($users);
+    }
+
+    public function show($id): JsonResponse
+    {
+        $user = User::findOrFail($id);
+        return response()->json($user);
+    }
+
+    public function store(Request $request): JsonResponse
+    {
+        $request->validate([
+            'name' => 'required|string|max:255',
+            'email' => 'required|string|email|max:255|unique:users',
+            'password' => 'required|string|min:8',
+        ]);
+
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => bcrypt($request->password), // Хешируем пароль
+        ]);
+
+        return response()->json($user, 201);
+    }
+
+    public function update(Request $request, $id): JsonResponse
+    {
+        $request->validate([
+            'name' => 'required|string|max:255',
+            'email' => 'required|string|email|max:255|unique:users,email,' . $id,
+            'password' => 'sometimes|string|min:8',
+        ]);
+
+        $user = User::findOrFail($id);
+        $user->update([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => $request->has('password') ? bcrypt($request->password) : $user->password,
+        ]);
+
+        return response()->json($user);
+    }
+
+    public function destroy($id): JsonResponse
+    {
+        $user = User::findOrFail($id);
+        $user->delete();
+        return response()->json(['message' => 'Пользователь удалён'], 200);
+    }
+}
--- php/routes/api.php
+++ php/routes/api.php
@@ -2,7 +2,8 @@
 
 use App\Http\Controllers\Api\BookController;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\UserController;
+use App\Http\Controllers\Api\UserController;
+
 
 Route::apiResource('books', BookController::class); //make 'index', 'show', 'store', 'update', 'destroy' methods.
-//Route::apiResource('users', UserController::class); //надо добавить контроллер под API,json.
+Route::apiResource('users', UserController::class); //надо добавить контроллер под API,json.
