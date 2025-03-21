18d9ac1b5d42a826da64ea1f4ac7287b77715421
TwoSug4r <nairoz0@mail.ru>
2025-03-21 04:05:51 +0300

    add some comments

--- php/routes/web.php
+++ php/routes/web.php
@@ -4,12 +4,17 @@ use App\Http\Controllers\BookController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
 
+//main routes
 Route::get('/', function () {
     return view('welcome');
 });
 
+
+//users routes
 Route::get('/users', [UserController::class, 'index']);
 
+
+//books routes
 Route::get('/books', [BookController::class, 'index'])->name('book.index');
 Route::get('/books/add', [BookController::class, 'create']);
 Route::post('/books', [BookController::class, 'store']); //POST для редиректа под добавление
