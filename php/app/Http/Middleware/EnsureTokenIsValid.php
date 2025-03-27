0b8f98eb8e11d318cf3c1cb187166751ec58713f
TwoSug4r <nairoz0@mail.ru>
2025-03-27 05:38:05 +0300

    add in return if don't have token 'with' with error key

--- php/app/Http/Middleware/EnsureTokenIsValid.php
+++ php/app/Http/Middleware/EnsureTokenIsValid.php
@@ -1,15 +1,9 @@
 <?php
 
- 
-
 namespace App\Http\Middleware;
 
- 
-
 use Closure;
-
 use Illuminate\Http\Request;
-
 use Symfony\Component\HttpFoundation\Response;
 
  
@@ -18,19 +12,15 @@ class EnsureTokenIsValid
 {
 
     /**
-
      * Handle an incoming request.
-
      *
-
      * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
-
      */
 
     public function handle(Request $request, Closure $next): Response //if token !== specified value then redirect in '/' 
     {
         if ($request->input('token') !== 'my-secret-token') {
-            return redirect('/');
+            return redirect('/')->with('error', 'you don\'t have token');
         }
 
         return $next($request);
