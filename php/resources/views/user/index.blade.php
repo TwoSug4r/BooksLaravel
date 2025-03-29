0b894f4e67db9cb287fc4700abb9fcb57b301a46
TwoSug4r <nairoz0@mail.ru>
2025-03-29 09:40:43 +0300

    add table with users from user.index

--- php/resources/views/user/index.blade.php
+++ php/resources/views/user/index.blade.php
@@ -3,6 +3,12 @@
 <head>
     <meta charset="UTF-8">
     <title>Users</title>
+    <style>
+       table {border-collapse: collapse; width: 100%; }
+       th, td { border: 1px solid #ddd; padding: 7px; text-align: center; }
+       th { background-color: #f2f2f2; }
+       button {padding: 5px 10px; margin-right: 5px;}
+    </style>
 </head>
 <body>
     <h1>List Users</h1>
@@ -13,4 +19,27 @@
     @endif
     <a href="/">Welcome page</a><br>
     <a href="/users/auth">Authorization</a><br>
+
+
+    <table>
+        <thead>
+            <th>ID</th>
+            <th>NAME</th>
+            <th>EMAIL</th>
+            <th>DELETE</th>
+        </thead>
+        <tbody>
+            @foreach ($users as $user)
+                <tr>
+                    <td> {{ $user->id }} </td>
+                    <td> {{ $user->name }} </td>
+                    <td> {{ $user->email }} </td>
+                    <td><form method="post" action="{{ route('users.destroy', $user->id) }}">
+                        @csrf
+                        @method('DELETE')
+                        <button type="submit">delete</button>
+                    </form></td>
+            @endforeach
+        </tbody>
+    </table>
 </body>
