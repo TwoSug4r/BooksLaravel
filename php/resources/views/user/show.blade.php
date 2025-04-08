9ca504a197ed69561288b5854c07f444d0931d42
TwoSug4r <nairoz0@mail.ru>
2025-04-08 17:04:27 +0300

    create show.blade for profile

---
+++ php/resources/views/user/show.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8" lang="eng">
+        <style>
+            table {border-collapse: collapse ; width: 100%;}
+            th,td {border: 1px solid #ddd ; text-align: center}
+        </style>
+    </head>
+    <body>
+
+    </body>
+</html>
