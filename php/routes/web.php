7f6689e72fb5150bffa512971ee1e3c851939813
TwoSug4r <nairoz0@mail.ru>
2025-04-08 17:09:50 +0300

    add breeze

--- php/routes/web.php
+++ php/routes/web.php
@@ -1,27 +1,20 @@
 <?php
 
-use App\Http\Controllers\BookController;
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\UserController;
 
-//main routes
 Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
 
-//users routes
-Route::get('/users', [UserController::class, 'index'])->name('user.index');
-Route::get('/users/register', [UserController::class, 'register']);
-Route::get('/profile', [UserController::class, ''])->middleware('auth');
-Route::post('/users', [UserController::class, 'store'])->name('user.register');
-Route::delete('/users/{id}', [UserController::class, 'destroy'])->name('user.destroy');
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
 
-//books routes
-Route::get('/books', [BookController::class, 'index'])->name('book.index');
-Route::get('/books/add', [BookController::class, 'create']);
-Route::post('/books', [BookController::class, 'store']); //POST для редиректа под добавление
-Route::get('/books/{id}/edit', [BookController::class, 'edit']);
-Route::put('/books/{id}', [BookController::class, 'update']);
-Route::delete('/books/{id}', [BookController::class, 'destroy'])->name('books.destroy');
-
+require __DIR__.'/auth.php';
