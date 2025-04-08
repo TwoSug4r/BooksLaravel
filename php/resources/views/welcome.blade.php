21bd397ddfd0db035996b3724594baba52f96ac0
TwoSug4r <nairoz0@mail.ru>
2025-04-08 17:33:21 +0300

    add button <login> welcome page

--- php/resources/views/welcome.blade.php
+++ php/resources/views/welcome.blade.php
@@ -3,8 +3,24 @@
 <head>
     <meta charset="UTF-8">
     <title>Welcome</title>
+    <style>
+        .buttonRightUp {
+            position: fixed;
+            text-decoration: none;
+            color: rgb(0, 0, 0);
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            right: 10px;
+            top: 10px;
+            background-color: #a01818;
+            padding: 5px;
+            width: 100px;
+        }
+    </style>
 </head>
 <body>
+    <a class="buttonRightUp" href="/login">Войти</a>
     <h1>Choose web</h1>
     <h2><a href="/books">Books</a></h2>
     <h2><a href="/users">Users</a><br><br></h2>
