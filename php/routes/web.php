10874501a660c0c795e7771c9e776db0d33b111e
TwoSug4r <nairoz0@mail.ru>
2025-04-12 08:27:00 +0300

    rework routes for books

--- php/routes/web.php
+++ php/routes/web.php
@@ -29,10 +29,12 @@ Route::post('/users', [UserController::class, 'store'])->name('user.register');
 Route::delete('/users/{id}', [UserController::class, 'destroy'])->name('user.destroy');
 
 //books routes
-Route::get('/books', [BookController::class, 'index'])->name('book.index');
-Route::get('/books/add', [BookController::class, 'create']);
-Route::post('/books', [BookController::class, 'store']); //POST для редиректа под добавление
-Route::get('/books/{id}/edit', [BookController::class, 'edit']);
-Route::put('/books/{id}', [BookController::class, 'update']);
-Route::delete('/books/{id}', [BookController::class, 'destroy'])->name('books.destroy');
+// Route::get('/books', [BookController::class, 'index'])->name('book.index');
+// Route::get('/books/add', [BookController::class, 'create']);
+// Route::post('/books', [BookController::class, 'store']); //POST для редиректа под добавление
+// Route::get('/books/{id}/edit', [BookController::class, 'edit']);
+// Route::put('/books/{id}', [BookController::class, 'update']);
+// Route::delete('/books/{id}', [BookController::class, 'destroy'])->name('books.destroy');
+
+Route::resource('books', BookController::class);
 
